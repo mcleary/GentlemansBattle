b467cce0907ad54c5145c350a9d4478ad1734bf3
mcleary <tluisrs@gmail.com>
2015-11-26 16:37:22 -0200

    Rascunho da documentação da estrutura de entrada

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 
 struct ModelInputData
 {
-    double start_army_size;             // > 0
-    double start_enemy_size;            // > 0
+    double start_army_size;             // > 0          Tamanho inicial do exército
+    double start_enemy_size;            // > 0          Tamanho inicial do exército inimigo
     double loose_battle_fraction;       // 0 < x < 1:   Porcentagem do exército que define derrota
-    double army_skill;                  // 0 < x <= 1
-    double enemy_skill;                 // 0 < x <= 1
-    double start_ammo;                  // > 0
-    double ammo_diffusion_coeffient;    // > 0
-    double battlefield_size;            // > 0
-    double front_line_fraction;         // 0 < x <= 1
+    double army_skill;                  // 0 < x <= 1   Perícia do exército em eliminar inimigos
+    double enemy_skill;                 // 0 < x <= 1   Perícia do inimigo em eliminar o exército
+    double start_ammo;                  // > 0          Quantidade de munição que estará disponível durante a batalha
+    double ammo_diffusion_coeffient;    // > 0          Velocidade com o que a munição é distribuída para o exército
+    double formation_size;              // > 0          Tamanho da formação utilizada na batalha pelo exército
+    double front_line_fraction;         // 0 < x <= 1   Porcentagem do exército que atuará na linha de frente
 
     double delta_time;                  // > 0
     double delta_x;                     // > 0
@@ -32,7 +32,7 @@ struct ModelInputData
         enemy_skill                 = 0.5;
         start_ammo                  = 1000;
         ammo_diffusion_coeffient    = 1.0;
-        battlefield_size            = 10;
+        formation_size            = 10;
         front_line_fraction         = 0.1;
 
         delta_time                  = 0.1;
@@ -61,7 +61,7 @@ struct ModelInfo
         old_army_size = input_data.start_army_size;
         old_enemy_size = input_data.start_enemy_size;
 
-        new_ammo_amount.resize(input_data.battlefield_size / input_data.delta_x);
+        new_ammo_amount.resize(input_data.formation_size / input_data.delta_x);
         old_ammo_amount.resize(new_ammo_amount.size());
     }
 
@@ -150,7 +150,7 @@ std::ostream& operator<< (std::ostream& out, const ModelInputData& input_data)
     out << "#- Enemy Skill        : " << input_data.enemy_skill << std::endl;
     out << "#- Start Ammo         : " << input_data.start_ammo << std::endl;
     out << "#- Ammo Diffusion Coef: " << input_data.ammo_diffusion_coeffient << std::endl;
-    out << "#- Battle Field Size  : " << input_data.battlefield_size << std::endl;
+    out << "#- Battle Field Size  : " << input_data.formation_size << std::endl;
     out << "#- Front Line Fraction: " << input_data.front_line_fraction << std::endl;
     out << "#- Delta Time         : " << input_data.delta_time << std::endl;
     out << "#- Delta X            : " << input_data.delta_x << std::endl;
