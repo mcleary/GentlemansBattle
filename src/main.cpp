7be5f2721e843d18d0dd6ad2c2d91eb30ab84996
mcleary <tluisrs@gmail.com>
2015-11-30 18:17:05 -0200

    Organização do código
    Parâmetro para dizer qual a taxa de tiros do exército, ou seja, qual a
    proporção da munição na linha de frente tem potencial para ser usada.
    
    Parâmetro para especificiar qual a porcentagem do inimigo irá atuar na
    linha de frente
    
    Código para imprimir o sumário da execução, bem com dizer quem ganhou ou
    perdeu
    
    Gráfico de difusão gerado da forma correta

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,11 @@ struct ModelInputData
     double army_skill;                  // 0 < x <= 1   Perícia do exército em eliminar inimigos
     double enemy_skill;                 // 0 < x <= 1   Perícia do inimigo em eliminar o exército
     double start_ammo;                  // > 0          Quantidade de munição que estará disponível durante a batalha
+    double army_fire_rate;              // 0 < x <= 1   Taxa com que o exército consegue atirar a munição disponível
     double ammo_diffusion_coeffient;    // > 0          Velocidade com o que a munição é distribuída para o exército
     double formation_size;              // > 0          Tamanho da formação utilizada na batalha pelo exército
     double front_line_fraction;         // 0 < x <= 1   Porcentagem do exército que atuará na linha de frente
+    double enemy_front_line_fraction;   // 0 < x <= 1   Porcentagem do exército inimigo que atuará na linha de frente
 
     double delta_time;                  // > 0
     double delta_x;                     // > 0
@@ -31,20 +33,43 @@ struct ModelInputData
      */
     ModelInputData()
     {
-        start_army_size             = 100.0;
-        start_enemy_size            = 100.0;
-        loose_battle_fraction       = 0.01;
-        army_skill                  = 0.001;
-        enemy_skill                 = 0.001;
-        start_ammo                  = 10;
-        ammo_diffusion_coeffient    = 0.5;
+        start_army_size             = 1000.0;
+        start_enemy_size            = 2500.0;
+        loose_battle_fraction       = 0.05;
+        army_skill                  = 0.05;
+        enemy_skill                 = 0.01;
+        start_ammo                  = 500;
+        army_fire_rate              = 0.5;
+        ammo_diffusion_coeffient    = 1.0;
         formation_size              = 10;
         front_line_fraction         = 0.1;
+        enemy_front_line_fraction   = 0.1;
 
-        delta_time                  = 0.01;
-        delta_x                     = 0.5;
+        delta_time                  = 0.07;
+        delta_x                     = 0.6;
 
-        std::cout << "Courant = " << ammo_diffusion_coeffient * delta_time / (delta_x * delta_x) << std::endl;
+        std::cout << "CFL = " << ammo_diffusion_coeffient * delta_time / (delta_x * delta_x) << std::endl;
+    }
+
+
+    friend std::ostream& operator<< (std::ostream& out, const ModelInputData& input_data)
+    {
+        out << "#-------------------------------------------------" << std::endl;
+        out << "#--- GentlesmanBattle Input Data" << std::endl;
+        out << "#-------------------------------------------------" << std::endl;
+        out << "#- Start Army Size    : " << input_data.start_army_size << std::endl;
+        out << "#- Start Enemy Size   : " << input_data.start_enemy_size << std::endl;
+        out << "#- Army Skill         : " << input_data.army_skill << std::endl;
+        out << "#- Enemy Skill        : " << input_data.enemy_skill << std::endl;
+        out << "#- Start Ammo         : " << input_data.start_ammo << std::endl;
+        out << "#- Ammo Diffusion Coef: " << input_data.ammo_diffusion_coeffient << std::endl;
+        out << "#- Battle Field Size  : " << input_data.formation_size << std::endl;
+        out << "#- Front Line Fraction: " << input_data.front_line_fraction << std::endl;
+        out << "#- Delta Time         : " << input_data.delta_time << std::endl;
+        out << "#- Delta X            : " << input_data.delta_x << std::endl;
+        out << "#--------------------------------------------------" << std::endl;
+
+        return out;
     }
 };
 
@@ -57,8 +82,6 @@ struct ModelInfo
 
     double new_enemy_size   = 0.0;
     double old_enemy_size   = 0.0;
-
-    double diffusion_sum = 0;
 
     std::vector<double> new_ammo_amount;
     std::vector<double> old_ammo_amount;
@@ -77,26 +100,26 @@ struct ModelInfo
         old_enemy_size = input_data.start_enemy_size;
 
         // Ammot starts at rear-line
-        old_ammo_amount[0] = model_input.start_ammo;
+        old_ammo_amount[1] = model_input.start_ammo;
     }
 
-    void advance_time(double delta_time)
+    void advance_time()
     {
         // Calculates the fraction of the army currently standing in the front-line
         double front_line_size = model_input.front_line_fraction * old_army_size;
         double enemy_front_line_size = model_input.front_line_fraction * old_enemy_size;
 
         // Army
-        new_army_size = old_army_size - delta_time * model_input.enemy_skill * front_line_size * enemy_front_line_size;
-        old_army_size = new_army_size;
+        new_army_size = old_army_size - model_input.delta_time * model_input.enemy_skill * front_line_size * enemy_front_line_size;
+        old_army_size = new_army_size;        
 
         // Enemy
-        double available_ammo = old_ammo_amount.back();
-        new_enemy_size = old_enemy_size - delta_time * model_input.army_skill * available_ammo * front_line_size;
+        double shoots_fired = old_ammo_amount.back() * model_input.army_fire_rate;
+        new_enemy_size = old_enemy_size - model_input.delta_time * model_input.army_skill * shoots_fired * front_line_size * old_enemy_size;
         old_enemy_size = new_enemy_size;
 
         // Ammo Diffusion
-        const double ddt_dx2 = model_input.ammo_diffusion_coeffient * delta_time / (model_input.delta_x * model_input.delta_x);
+        const double ddt_dx2 = model_input.ammo_diffusion_coeffient * model_input.delta_time / (model_input.delta_x * model_input.delta_x);
         for(size_t i = 1; i < new_ammo_amount.size() - 1; ++i)
         {
             new_ammo_amount[i] = old_ammo_amount[i] + ddt_dx2 * (old_ammo_amount[i-1] - 2.0 * old_ammo_amount[i] + old_ammo_amount[i+1]);
@@ -105,16 +128,19 @@ struct ModelInfo
         //
         // Ammo boundary conditions
         //
+
         // In x=0 there is no flow.
         new_ammo_amount[0] = new_ammo_amount[1];
+
         // In x=L the ammo is being used by the soldiers
-        new_ammo_amount.back() = new_ammo_amount[new_ammo_amount.size() - 2] * model_input.front_line_fraction;
+        double ammo_usage_ratio = 1.0 - (1.0 / front_line_size);
+        new_ammo_amount.back() = new_ammo_amount[new_ammo_amount.size() - 2] * ammo_usage_ratio;
 
         // Swap vectors for next time step
         new_ammo_amount.swap(old_ammo_amount);
 
         // Finally, advance the time
-        time += delta_time;
+        time += model_input.delta_time;
     }
 
     /**
@@ -131,9 +157,43 @@ struct ModelInfo
         return new_army_size <= model_input.start_army_size * model_input.loose_battle_fraction ||
                 new_enemy_size <= model_input.start_enemy_size * model_input.loose_battle_fraction;
     }
-};
 
-std::ostream& operator<< (std::ostream& out, const ModelInputData& input_data);
+    /**
+     * @brief Returns true if the number of soldiers is bigger than the number of enemies soldiers at this moment
+     */
+    bool is_army_winning() const
+    {
+        return old_army_size > old_enemy_size;
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const ModelInfo& info)
+    {
+        out << "#-------------------------------------------------" << std::endl;
+        out << "#--- GentlesmanBattle Execution Summary" << std::endl;
+        out << "#-------------------------------------------------" << std::endl;
+        out << "# Number of Iterations      : " << info.time / info.model_input.delta_time << std::endl;
+        out << "# Total time                : " << info.time << std::endl;
+        out << "# Soldiers Count            : " << info.new_army_size << std::endl;
+        out << "# Enemies Count             : " << info.new_enemy_size << std::endl;
+        out << "# Available Ammo at Fronline: " << info.new_ammo_amount.back() << std::endl;
+        out << "# Available Ammo at Rearline: " << info.new_ammo_amount.front() << std::endl;
+        out << "#-------------------------------------------------" << std::endl;
+        out << "# Battle Result: ";
+
+        if(info.is_army_winning())
+        {
+            out << "YOU WIN!" << std::endl;
+        }
+        else
+        {
+            out << "YOU LOOSE!" << std::endl;
+        }
+
+        out << "#-------------------------------------------------" << std::endl;
+
+        return out;
+    }
+};
 
 struct ModelOutput
 {
@@ -146,45 +206,41 @@ struct ModelOutput
           model_info(_model_info)
     {
         output_file.open(model_output_filename, std::ios::out);
-
-        if(!output_file.is_open())
-        {
-            throw std::runtime_error("Cannot open outputfile: " + model_output_filename);;
-        }
-
         output_file << model_input << std::endl << std::endl;
         output_file << "# Time  ArmySize    EnemySize" << std::endl;
     }
 
-    void write_output()
+    void write_output_step()
     {
         output_file << std::setw(10) << std::setprecision(10) << std::fixed << std::setfill('0') <<
                        model_info.time << " " <<
                        model_info.old_army_size << " " <<
-                       model_info.old_enemy_size << " " <<
-                       model_info.old_ammo_amount[model_info.old_ammo_amount.size() - 1] << " " <<
-                       model_info.old_ammo_amount[0] << " " <<
-                       model_info.diffusion_sum <<
-
+                       model_info.old_enemy_size << " " <<                       
+                       model_info.old_ammo_amount.back() << " " <<
                        std::endl;
     }
 
-    void show_gnuplot()
+    void stop_execution(bool b_show_gnuplot = true)
     {
+        // Write Execution Summary into the output file
+        output_file << model_info << std::endl;
+
         if(output_file.is_open())
         {
             output_file.close();
         }
 
-        std::string output_filename_quotes = "'" + model_output_filename + "'";
-        std::string plot_command = "gnuplot -p -e \"plot " +
-                output_filename_quotes + " using 1:2 with lines title 'Soldiers Amount'," +
-                output_filename_quotes + " using 1:3 with lines title 'Enemies Amount'," +
-                output_filename_quotes + " using 1:4 with lines title 'Ammo at front-line'," +
-                output_filename_quotes + " using 1:5 with lines title 'Ammo at rear-guard'" +
-                "\"";
-        std::cout << plot_command << std::endl;
-        system(plot_command.data());
+        if(b_show_gnuplot)
+        {
+            std::string output_filename_quotes = "'" + model_output_filename + "'";
+            std::string plot_command = "gnuplot -p -e \"plot " +
+                    output_filename_quotes + " using 1:2 with lines title 'Soldiers Amount'," +
+                    output_filename_quotes + " using 1:3 with lines title 'Enemies Amount'," +
+                    output_filename_quotes + " using 1:4 with lines title 'Ammo at rear-guard'" +
+                    "\"";
+            std::cout << plot_command << std::endl;
+            system(plot_command.data());
+        }
     }
 };
 
@@ -195,31 +251,34 @@ struct GentlesmanBattleModel
     ModelInfo info;
     int iteration = 0;
 
-    std::function<void(const GentlesmanBattleModel&)> step_callback = nullptr;
+    typedef std::function<void()> ModelCallbackFunc;
+
+    ModelCallbackFunc step_callback = nullptr;
 
     GentlesmanBattleModel() :
         output("", info, input), info(input)
     {
     }
 
-    int get_num_ticks() const
-    {
-        return static_cast<int>(std::ceil(info.time / input.delta_time));
-    }
-
     void run()
     {
+        // Print parameters information
+        std::cout << input << std::endl;
+
         do
         {
-            output.write_output();
-            info.advance_time(input.delta_time);
+            output.write_output_step();
+            info.advance_time();
             if(step_callback)
             {
-                step_callback(*this);
+                step_callback();
             }
             iteration++;
         }
         while(!info.should_stop());
+
+        // Print execution summary
+        std::cout << info << std::endl;
     }
 };
 
@@ -241,7 +300,7 @@ void init_plot()
     QObject::connect(custom_plot->yAxis, SIGNAL(rangeChanged(QCPRange)), custom_plot->yAxis2, SLOT(setRange(QCPRange)));
 }
 
-void add_plot(const GentlesmanBattleModel& model)
+void add_diffusion_plot(const GentlesmanBattleModel& model)
 {
     // add two new graphs and set their look:
     static int graph_index = 0;
@@ -255,15 +314,14 @@ void add_plot(const GentlesmanBattleModel& model)
     // Convert model data to QCustomPlot format (QVector)
     //
 
-    // Generate y-axis
+    // Generate y-axis10
     QVector<double> y_axis = QVector<double>::fromStdVector(model.info.new_ammo_amount);
 
-    // Generate x-axis
-    int num_time_ticks = model.get_num_ticks();
-    QVector<double> x_axis(num_time_ticks);
-    for(int i_tick = 0; i_tick < num_time_ticks; ++i_tick)
+    // Generate x-axis    
+    QVector<double> x_axis(y_axis.size());
+    for(int i = 0; i < x_axis.size(); ++i)
     {
-        x_axis[i_tick] = i_tick * model.input.delta_time;
+        x_axis[i] = i;
     }
 
     // pass data points to graphs:
@@ -279,7 +337,7 @@ void add_plot(const GentlesmanBattleModel& model)
 void show_plot()
 {
     // Note: we could have also just called custom_plot->rescaleAxes(); instead
-    // Allow user to drag axis ranges with mouse, zoom with mouse wheel and select graphs by clicking:
+    // Allow user to drag axis rainfonges with mouse, zoom with mouse wheel and select graphs by clicking:
     custom_plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
 
     custom_plot->setGeometry(20, 20, 800, 800);
@@ -290,21 +348,19 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    init_plot();
-
     GentlesmanBattleModel model;
 
-    model.step_callback = [&](const GentlesmanBattleModel&)
+    init_plot();
+
+    model.step_callback = [&]()
     {
         static int num_plots = 0;
-        if(num_plots < 100)
+        if(num_plots < 1000)
         {
-            if((model.iteration) % 2000 == 0)
+            if((model.iteration) % 100 == 0)
             {
-                add_plot(model);
-                std::cout << "Iteration: " << model.iteration << std::endl;
+                add_diffusion_plot(model);
                 num_plots++;
-                custom_plot->rescaleAxes();
             }
         }
     };
@@ -313,27 +369,8 @@ int main(int argc, char *argv[])
 
     model.run();
 
+    model.output.stop_execution();
+
     return app.exec();
-}
-
-
-std::ostream& operator<< (std::ostream& out, const ModelInputData& input_data)
-{
-    out << "#-------------------------------------------------" << std::endl;
-    out << "#--- GentlesmanBattle Input Data" << std::endl;
-    out << "#-------------------------------------------------" << std::endl;
-    out << "#- Start Army Size    : " << input_data.start_army_size << std::endl;
-    out << "#- Start Enemy Size   : " << input_data.start_enemy_size << std::endl;
-    out << "#- Army Skill         : " << input_data.army_skill << std::endl;
-    out << "#- Enemy Skill        : " << input_data.enemy_skill << std::endl;
-    out << "#- Start Ammo         : " << input_data.start_ammo << std::endl;
-    out << "#- Ammo Diffusion Coef: " << input_data.ammo_diffusion_coeffient << std::endl;
-    out << "#- Battle Field Size  : " << input_data.formation_size << std::endl;
-    out << "#- Front Line Fraction: " << input_data.front_line_fraction << std::endl;
-    out << "#- Delta Time         : " << input_data.delta_time << std::endl;
-    out << "#- Delta X            : " << input_data.delta_x << std::endl;
-    out << "#--------------------------------------------------" << std::endl;
-
-    return out;
 }
 
